34fe3a0e160ca3939bc9570a605141818b0ac0c8
Vaibhav Singh <singhvaibhav654@gmail.com>
2024-09-04 03:06:55 +0530

    toast

--- app/user1/page.jsx
+++ app/user1/page.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const page = ({ searchParams }) => {
@@ -5,8 +6,9 @@ const page = ({ searchParams }) => {
   return (
     <div className={`${""} text-black font-bold text-5xl bg-red-500`}>
       user1 text-5xl font-bold bg-red-600 pathname = {pathname}
+      <Link href="/token" />
     </div>
   );
 };
 
-export default page;  
+export default page;
---
+++ app/user1/token/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>token</div>;
+};
+
+export default page;
