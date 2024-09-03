aff0af9bc0590245845f2efce2f79af20ea8d9ba
Vaibhav Singh <singhvaibhav654@gmail.com>
2024-09-04 01:25:19 +0530

    sjkfdnvieie

--- middleware.js
+++ middleware.js
@@ -10,6 +10,9 @@ export async function middleware(request) {
     hostname !== "localhost" &&
     hostname !== "rewrites-tailwind-custom-domain.vercel.app"
   ) {
+    if (pathname.startsWith("/")) {
+      return NextResponse.rewrite(new URL(`/user1`, request.url));
+    }
     return NextResponse.rewrite(new URL(`/user1`, request.url));
   } else if (pathname.startsWith("/go")) {
     return NextResponse.rewrite(new URL(`/user1`, request.url));
